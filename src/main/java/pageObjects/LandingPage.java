d74a9a972a550192461c8e978a56d1ab3c9e87ee
gabrielele908 <48172569+gabrielele908@users.noreply.github.com>
2024-07-14 21:44:31 +0300

    Update LandingPage.java
    any

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -1,9 +1,8 @@
 package pageObjects;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//rahulonlinetutor@gmail.com
+
 public class LandingPage {
 
 	
